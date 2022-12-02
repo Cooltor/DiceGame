cedbd1f33be78f8430cbb8389194e58891de3027
Cooltor <romaindbus86@gmail.com>
2022-12-02 10:18:50 +0100

    Btn Again done

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player1El.classList.remove('player--winner');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('activePlayer');
+  player0El.classList.add('activePlayer');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -79,18 +92,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-
-  current0El = 0;
-  current1El = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  document.querySelector(`.player--0`).classList.remove('player--winner');
-  document.querySelector(`.player--1`).classList.remove('player--winner');
-  playing = true;
-});
+btnNew.addEventListener('click', init);
